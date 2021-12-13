315b39adb07f22a99bbf7127d373a1eb94613ea4
Riccardo <riccardolipparini0@gmail.com>
2021-12-13 14:57:31 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,13 @@ export default {
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   //text-align: center;
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,6 +1,13 @@
 <template>
-  <div id="app">
-    
+  <div id="container">
+    <!-- menu -->
+    <div class="centred">
+      <h1>NO PAIN NO GAIN</h1>
+      <p>Motivation is whats gets you started. Habit is what keeps you going</p>
+      <div class="buttons">
+        <a href="">JOIN US TODAY</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,13 +15,46 @@
 //import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'MyHeader',
-  components: {
-    
-  }
-}
+  name: "MyHeader",
+  components: {},
+};
 </script>
 
 <style scoped lang="scss">
-
+#container {
+  position: relative;
+  height: 100vh;
+  background-image: url("../assets/img/home-banner.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  .centred {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -40%);
+    text-align: center;
+    width: 80%;
+    h1 {
+      color: white;
+      font-size: 100px;
+      padding: 0px;
+    }
+    p {
+      color: rgba(254, 254, 254, 255);
+      margin-top: 20px;
+    }
+    .buttons {
+      padding: 20px;
+      border: solid rgba(174, 175, 174, 255) 1px;
+      width: 150px;
+      margin: 0 auto;
+      margin-top: 40px;
+      cursor: pointer;
+      a {
+        text-decoration: none;
+        color: rgba(174, 175, 174, 255);
+      }
+    }
+  }
+}
 </style>
