dbc6cd50c0078ff4887af6d09f234a59439f8b96
Riccardo <riccardolipparini0@gmail.com>
2021-12-13 12:37:58 +0100

    first commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'MyHeader',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'MyMain',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+ 
+
+</style>
