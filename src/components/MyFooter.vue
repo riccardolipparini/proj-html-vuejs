5d2905225987f7b2de4e20cdd565d71e696b212e
Riccardo <riccardolipparini0@gmail.com>
2021-12-14 13:58:04 +0100

    add first part footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,5 +1,35 @@
 <template>
-  <div id="app">
+  <div id="container">
+    <div class="contain_footer">
+      <div class="info_footer">
+        <h4>COME FIND US</h4>
+        <p>blblblb</p>
+        <p>blblblb</p>
+        <p>blblbl</p>
+        <p>nvnvnvnv</p>
+      </div>
+
+      <div class="info_footer">
+        <h4>COME FIND US</h4>
+        <p>blblblb</p>
+        <p>blblblb</p>
+        <p>blblbl</p>
+        <p>nvnvnvnv</p>
+      </div>
+
+      <div class="info_footer">
+        <h4>COME FIND US</h4>
+        <p>blblblb</p>
+        <p>blblblb</p>
+        <p>blblbl</p>
+        <p>nvnvnvnv</p>
+      </div>
+
+      <div class="info_footer">
+        <img src="../assets/img/gym_ad-compressor.jpg" alt="">
+      </div>
+      
+    </div>
     
   </div>
 </template>
@@ -16,5 +46,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  background-color: rgba(31,33,35,255);
+  .contain_footer {
+    width: 60%;
+    display: flex;
+    .info_footer{
+      padding-right:50px;
+    }
+    h4{
+      color: rgba(50,55,58,255);
+      margin-bottom: 30px;
+    }
+    p{
+      color: rgba(121,122,122,255);
+    }
+  }
+}
 </style>
