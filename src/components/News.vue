3831ae3a1976af780b33ed581fb53ed48fc7d8ea
Riccardo <riccardolipparini0@gmail.com>
2021-12-15 16:30:56 +0100

    add dynamics

--- src/components/News.vue
+++ src/components/News.vue
@@ -6,35 +6,15 @@
         <img src="../assets/img/divider-xx-red.png" alt="" />
       </div>
       <div class="results">
-        <div class="contain_news">
-          <div class="news">
-            <img src="../assets/img/blog1-400x208.jpg" alt="" />
+        
+          <div class="news" v-for="info, i in news" :key="i">
+            <img :src="require(`../assets/img/${info.image}`)" alt="" />
             <div class="news_space">
-              <h1>titolo</h1>
-              <p>text</p>
+              <h1>{{info.title}}</h1>
+              <p>{{info.text}}</p>
             </div>
           </div>
-        </div>
-
-        <div class="contain_news">
-          <div class="news">
-            <img src="../assets/img/blog1-400x208.jpg" alt="" />
-            <div class="news_space">
-              <h1>titolo</h1>
-              <p>text</p>
-            </div>
-          </div>
-        </div>
-
-        <div class="contain_news">
-          <div class="news">
-            <img src="../assets/img/blog1-400x208.jpg" alt="" />
-            <div class="news_space">
-              <h1>titolo</h1>
-              <p>text</p>
-            </div>
-          </div>
-        </div>
+        
       </div>
       <div class="buttons">
         <a href="">READ ALL ARTICLES</a>
@@ -48,7 +28,42 @@
 
 export default {
   name: "News",
-  components: {},
+  data(){
+    return{
+      news:[
+        {
+          text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title:"Train with free weights or your body weights?",
+          image:"blog1-400x208.jpg"
+        },
+        {
+          text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title:"Nutritional advice that will keep you training",
+          image:"blog6-600x600.jpg"
+        },
+        {
+          text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title:"Simple principles for your next workout",
+          image:"blog4-400x289.jpg"
+        },
+        {
+          text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title:"To be number one, training like you're number two",
+          image:"trainer1.jpg"
+        },
+        {
+          text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title:"Top 5 mistakes that every gym member makes",
+          image:"trainer3.jpg"
+        },
+        {
+          text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          title:"The myths of shedding body fat explored",
+          image:"trainer4.jpg"
+        },
+      ]
+    }
+  }
 };
 </script>
 
@@ -57,7 +72,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 1500px;
   background-color: rgba(37, 41, 43, 255);
 
   .contain_results {
@@ -65,35 +80,43 @@ export default {
   }
   .title {
     text-align: center;
+    img{
+      margin-top: 40px;
+      margin-bottom: 20px;
+    }
+    h1{
+      color: white;
+    }
   }
   .results {
     display: flex;
     justify-content: space-between;
-    .contain_news {
-      width: calc(90% / 3);
-      margin: 20px 20px;
+    flex-wrap: wrap;
       .news {
-        margin-bottom: 50px;
+        width: calc(90% / 3);
+        margin-bottom: 30px;
         background-color: rgba(31, 33, 35, 255);
         .news_space{
           margin: 0 auto;
           width: 80%;
         }
         img {
-          max-width: 100%;
+          width: 100%;
         }
         h1 {
           color: white;
           margin-top: 10px;
+          font-size: 20px;
         }
         p {
           color: rgba(128, 128, 129, 255);
           margin-top: 30px;
+          margin-bottom: 30px;
         }
       }
-    }
   }
 }
+
 .buttons {
   padding: 10px;
   border: solid rgba(174, 175, 174, 255) 1px;
