6842b4d4cece6842df16943ae158fe05a5cc4f51
Riccardo <riccardolipparini0@gmail.com>
2021-12-15 17:54:57 +0100

    es finito

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { faHeart, faUnlink, faBicycle, faCloud } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faUnlink, faBicycle, faCloud, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 library.add(faFacebook, faTwitter, faInstagram)
-library.add(faHeart, faUnlink, faBicycle, faCloud)
+library.add(faHeart, faUnlink, faBicycle, faCloud, faSearch)
 
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
